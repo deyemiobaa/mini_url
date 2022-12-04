5d8ac696384e39099020e704b46e7e3b617c8d0c
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-12-04 08:44:27 +0100

    write model spec to test validations 🧪

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    link = Link.new(
+      long_url: "https://www.google.com/this_is_my_custom_long_url", 
+      short_url: "http://www.miniurl.com/short_url"
+    )
+    expect(link).to be_valid
+  end
+
+  it "is not valid without a long_url" do
+    link = Link.new(
+      long_url: nil, 
+      short_url: "http://www.miniurl.com/short_url"
+    )
+    expect(link).to_not be_valid
+  end
+
+  it "is not valid without a short_url" do
+    link = Link.new(
+      long_url: "https://www.google.com/this_is_my_custom_long_url", 
+      short_url: nil
+    )
+    expect(link).to_not be_valid
+  end
 end
--- spec/models/path_spec.rb
+++ spec/models/path_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Path, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    path = Path.new(
+      value: "this_is_my_custom_path"
+    )
+    expect(path).to be_valid
+  end
+
+  it "is not valid without a value" do
+    path = Path.new(
+      value: nil
+    )
+    expect(path).to_not be_valid
+  end
+
+  it "is not valid with a duplicate value" do
+    path = Path.new(
+      value: "this_is_my_custom_path"
+    )
+    path.save
+    path2 = Path.new(
+      value: "this_is_my_custom_path"
+    )
+    expect(path2).to_not be_valid
+  end
 end
