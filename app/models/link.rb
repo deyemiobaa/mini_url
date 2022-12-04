8d2a12bd20ec908e42cb1538b1321391f125aa65
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-12-04 08:35:26 +0100

    add model validations ✔

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,9 @@
 class Link < ApplicationRecord
+  validates :long_url, presence: true
+  validates :short_url, presence: true, uniqueness: true
+
+  def update_click_count
+    self.clicks += 1
+    self.save
+  end
 end
--- app/models/path.rb
+++ app/models/path.rb
@@ -1,2 +1,3 @@
 class Path < ApplicationRecord
+  validates :value, presence: true, uniqueness: true
 end
